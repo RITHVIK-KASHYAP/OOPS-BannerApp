b0834707ffdbe52ef32a2fc468407ba1114c42bc
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-02-17 16:04:57 +0530

    Coded UC2 to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/*
+OOPSBAnner App UC1 - OOPS Banner Display
+
+this clas demonstrates a simple java application that displays the object
+oriented programming system OOPS acronym to the console
+
+@author Rithvik
+@version 2.0
+
+*/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("    *****    "+"    *****    "+" ************"+" **************");
+        System.out.println("  **     **  "+"  **     **  "+" **        **"+" **            ");
+        System.out.println(" **       ** "+" **       ** "+" **        **"+" **            ");
+        System.out.println(" **       ** "+" **       ** "+" ************"+" **************");
+        System.out.println(" **       ** "+" **       ** "+" **          "+"             **");
+        System.out.println("  **     **  "+"  **     **  "+" **          "+"             **");
+        System.out.println("    *****    "+"    *****    "+" **          "+" **************");
+
+    }
+}
