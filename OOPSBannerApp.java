3dc31dd9d31d7a8df6835177bafc925e47e7a038
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-02-18 16:30:19 +0530

    USE CASE 5 OOPS BANNER DISPLAY

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,27 +1,30 @@
 /*
-OOPSBAnner App UC4 - OOPS Banner Display
+OOPSBAnner App UC5 - OOPS Banner Display
 
 this clas demonstrates a simple java application that displays the object
 oriented programming system OOPS acronym to the console
 
 @author Rithvik
-@version 4.0
+@version 5.0
 
 */
 public class OOPSBannerApp {
+   
     public static void main(String[] args) {
-        String arr[] = new String[7];
-        arr[0]=(String.join("","    *****    ","    *****    "," ************"," **************"));
-        arr[1]=(String.join("","  **     **  ","  **     **  "," **        **"," **            "));
-        arr[2]=(String.join(""," **       ** "," **       ** "," **        **"," **            "));
-        arr[3]=(String.join(""," **       ** "," **       ** "," ************"," **************"));
-        arr[4]=(String.join(""," **       ** "," **       ** "," **          ","             **"));
-        arr[5]=(String.join("","  **     **  ","  **     **  "," **          ","             **"));
-        arr[6]=(String.join("","    *****    ","    *****    "," **          "," **************"));
+        String names[]=
+        {"    *****        *****     ************ **************",
+        "  **     **    **     **   **        ** **            ",
+        " **       **  **       **  **        ** **            ",
+        " **       **  **       **  ************ **************",
+        " **       **  **       **  **                       **",
+        "  **     **    **     **   **                       **",
+        "    *****        *****     **           **************"};
 
-        for(int i=0;i<arr.length;i++)
+        for(String name:names)
         {
-            System.out.println(arr[i]);
+            System.out.println(name);
         }
     }
 }
+
+
