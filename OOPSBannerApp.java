124a4e97c64c0c2d50a0a14834eca97cf6be164d
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-02-18 17:15:46 +0530

    UC6

---
+++ OOPSBannerApp.java
@@ -0,0 +1,63 @@
+/*
+OOPSBAnner App UC6 - OOPS Banner Display
+
+this clas demonstrates a simple java application that displays the object
+oriented programming system OOPS acronym to the console
+
+@author Rithvik
+@version 6.0
+
+*/
+public class OOPSBannerApp {
+    public static String[] getOPattern()
+    {
+        return new String[]
+        {
+        "    *****    ",
+        "  **     **  ",
+        " **       ** ",
+        " **       ** ",
+        " **       ** ",
+        "  **     **  ",
+        "    *****    "
+        };
+    }
+    public static String[] getPPattern()
+    {
+        return new String[]
+        {
+        "************ ",
+        "**        ** ",
+        "**        ** ",
+        "************ ",
+        "**           ",
+        "**           ",
+        "**           "
+    };
+    }
+    public static String[] getSPattern()
+    {
+        return new String[]
+        {
+        "**************",
+        "**            ",
+        "**            ",
+        "**************",
+        "            **",
+        "            **",
+        "**************"
+    };
+    }
+    public static void main(String[] args) {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getOPattern();
+        String[] sPattern = getOPattern();
+
+        for(int i=0 ; i<oPattern.length;i++)
+        {
+            System.out.println(oPattern[i]+" "+oPattern[i]+" "+pPattern[i]+" "+sPattern[i]);
+        }
+    }
+}
+
+
