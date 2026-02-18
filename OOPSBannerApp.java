f3358bf2ea65dcb49f45af8a32e0b6dfedd586b8
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-02-18 10:11:02 +0530

    feature/UC: Print OOPS

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -8,7 +8,7 @@ oriented programming system OOPS acronym to the console
 @version 1.0
 
 */
-public class HelloWorld {
+public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println("OOPS");
     }
