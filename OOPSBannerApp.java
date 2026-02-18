800e0624526646f93c827bd4de10ad04ddb1d446
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-02-18 16:28:20 +0530

    USE CASE 3 OOPS BANNER DISPLAY

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,23 @@
 /*
-OOPSBAnner App UC2 - OOPS Banner Display
+OOPSBAnner App UC3 - OOPS Banner Display
 
 this clas demonstrates a simple java application that displays the object
-oriented programming system OOPS acronym to the console
+oriented programming system OOPS acronym to the console 
 
 @author Rithvik
-@version 2.0
+@version 3.0
 
 */
 public class OOPSBannerApp
  {
     public static void main(String[] args) {
-        System.out.println("    *****    "+"    *****    "+" ************"+" **************");
-        System.out.println("  **     **  "+"  **     **  "+" **        **"+" **            ");
-        System.out.println(" **       ** "+" **       ** "+" **        **"+" **            ");
-        System.out.println(" **       ** "+" **       ** "+" ************"+" **************");
-        System.out.println(" **       ** "+" **       ** "+" **          "+"             **");
-        System.out.println("  **     **  "+"  **     **  "+" **          "+"             **");
-        System.out.println("    *****    "+"    *****    "+" **          "+" **************");
+        System.out.println(String.join("","    *****    ","    *****    "," ************"," **************"));
+        System.out.println(String.join("","  **     **  ","  **     **  "," **        **"," **            "));
+        System.out.println(String.join(""," **       ** "," **       ** "," **        **"," **            "));
+        System.out.println(String.join(""," **       ** "," **       ** "," ************"," **************"));
+        System.out.println(String.join(""," **       ** "," **       ** "," **          ","             **"));
+        System.out.println(String.join("","  **     **  ","  **     **  "," **          ","             **"));
+        System.out.println(String.join("","    *****    ","    *****    "," **          "," **************"));
 
     }
 }
