a4d19a5fd3db5890bac520b061610643fb0dd262
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-02-18 14:42:43 +0530

    Release Version 4.0

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,27 @@
 /*
-OOPSBAnner App UC3 - OOPS Banner Display
+OOPSBAnner App UC4 - OOPS Banner Display
 
 this clas demonstrates a simple java application that displays the object
-oriented programming system OOPS acronym to the console 
+oriented programming system OOPS acronym to the console
 
 @author Rithvik
-@version 2.0
+@version 4.0
 
 */
-public class OOPSBannerApp
- {
+public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println(String.join("","    *****    ","    *****    "," ************"," **************"));
-        System.out.println(String.join("","  **     **  ","  **     **  "," **        **"," **            "));
-        System.out.println(String.join(""," **       ** "," **       ** "," **        **"," **            "));
-        System.out.println(String.join(""," **       ** "," **       ** "," ************"," **************"));
-        System.out.println(String.join(""," **       ** "," **       ** "," **          ","             **"));
-        System.out.println(String.join("","  **     **  ","  **     **  "," **          ","             **"));
-        System.out.println(String.join("","    *****    ","    *****    "," **          "," **************"));
+        String arr[] = new String[7];
+        arr[0]=(String.join("","    *****    ","    *****    "," ************"," **************"));
+        arr[1]=(String.join("","  **     **  ","  **     **  "," **        **"," **            "));
+        arr[2]=(String.join(""," **       ** "," **       ** "," **        **"," **            "));
+        arr[3]=(String.join(""," **       ** "," **       ** "," ************"," **************"));
+        arr[4]=(String.join(""," **       ** "," **       ** "," **          ","             **"));
+        arr[5]=(String.join("","  **     **  ","  **     **  "," **          ","             **"));
+        arr[6]=(String.join("","    *****    ","    *****    "," **          "," **************"));
 
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
     }
 }
