7892c18be8b1ada38c69fffd4fd3f0525ecf6fbe
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-02-18 14:35:01 +0530

    Release Version 1.0

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/*
+OOPSBAnner App UC1 - OOPS Banner Display
+
+this clas demonstrates a simple java application that displays the object
+oriented programming system OOPS acronym to the console
+
+@author Rithvik
+@version 1.0
+
+*/
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
